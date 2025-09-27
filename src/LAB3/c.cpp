b7c6552f94c87b22f7923d17cbbdea4e01c44986
Don Vladimir <donnvladimir70@gmail.com>
2025-09-27 09:25:45 +0500

    lab 3 c my own impl

--- src/LAB3/c.cpp
+++ src/LAB3/c.cpp
@@ -1,36 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+long long countInRange(const vector<long long>& A, long long l, long long r) {
+    long long cnt = 0;
+    for (long long x : A) {
+        if (x >= l && x <= r) cnt++;
+    }
+    return cnt;
+}
 
+
+int main() {
     int n, q;
     cin >> n >> q;
     vector<long long> A(n);
-    for (int i = 0; i < n; i++) cin >> A[i];
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
 
     sort(A.begin(), A.end());
 
-    while (q--) {
+    while(q--) {
         long long l1, r1, l2, r2;
         cin >> l1 >> r1 >> l2 >> r2;
 
-        auto c1 = upper_bound(A.begin(), A.end(), r1)
-                - lower_bound(A.begin(), A.end(), l1);
-
-        auto c2 = upper_bound(A.begin(), A.end(), r2)
-                - lower_bound(A.begin(), A.end(), l2);
+        auto c1 = countInRange(A, l1, r1);
+        auto c2 = countInRange(A, l2, r2);
 
         long long L = max(l1, l2);
         long long R = min(r1, r2);
-        long long cOverlap = 0;
+        long long c0verlap = 0;
+
         if (L <= R) {
-            cOverlap = upper_bound(A.begin(), A.end(), R)
-                     - lower_bound(A.begin(), A.end(), L);
+            c0verlap = countInRange(A, max(l1,l2), min(r1,r2));
         }
 
-        cout << (c1 + c2 - cOverlap) << '\n';
+        cout << (c1 + c2 - c0verlap) << '\n';
     }
-
-    return 0;
 }
